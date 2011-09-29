8281dcc46c7b786e495c889ce9fed1c1c81a7f43
SuperHutagalungX <bataxx_keren@yahoo.co.uk>
2011-09-29 14:40:25 +0700

    Still need to change.

---
+++ mysqlKonek.vb
@@ -0,0 +1,16 @@
+﻿Imports MySql.Data.MySqlClient
+Module mysqlKonek
+    Private Conn As MySqlConnection = Nothing
+    Public Function Koneksi_mysql() As MySqlConnection
+        Dim ConnString As String
+        ConnString = ";server=localhost" & ";user=root" & ";password=admin" & ";database=lapsid"
+        Try
+            Conn = New MySqlConnection(ConnString)
+            Conn.Open()
+        Catch ex As MySqlException
+            MessageBox.Show("Koneksi Erros: " + ex.Message)
+        End Try
+        Return Conn
+    End Function
+End Module
+
